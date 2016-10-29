a5c0b2048894d213214e86e818703031735437f4
Renzo Crisóstomo <ruenzuo@gmail.com>
2016-10-29 18:30:02 +0200

    Add FanSabisuKitLite

---
+++ FanSabisuKitLite/Other sources/FanSabisuKitLite.h
@@ -0,0 +1,19 @@
+//
+//  FanSabisuKitLite.h
+//  FanSabisuKitLite
+//
+//  Created by Renzo Crisóstomo on 29/10/2016.
+//  Copyright © 2016 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FanSabisuKitLite.
+FOUNDATION_EXPORT double FanSabisuKitLiteVersionNumber;
+
+//! Project version string for FanSabisuKitLite.
+FOUNDATION_EXPORT const unsigned char FanSabisuKitLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FanSabisuKitLite/PublicHeader.h>
+
+
